0f7c93d217b5823cc8520f89a07d1a7c65394b82
Johann Molano <jjdmolano@gmail.com>
2020-09-08 19:09:38 +0800

    Added gitignore

--- src/com/zuitt/app/Car.java
+++ src/com/zuitt/app/Car.java
@@ -2,14 +2,20 @@ package com.zuitt.app;
 
 public class Car {
     double fuel = 0;
-    double fuelMaxLimit = 45000;
+    double fuelMaxLimit = 45    ;
     double mileage = 0;
 
     public void drive(double distanceInMeters) {
 //        20km = 1L
+//        add to mileage
+//        consume fuel
+        System.out.println("Your new mileage is ");
+        System.out.println("Your new fuel consumption is ");
     }
 
-    public void refuel() {
-
+    public void refuel(double fuelAmount) {
+//        add to fuel
+        System.out.println("You refueled " + " liters");
+        System.out.println("Total fuel in tank is " + " liters");
     }
 }
